22055ad61e34b6496702f996cf6e851ba7dc38d3
Giulia <giuliafloris88@gmail.com>
2022-06-24 09:42:18 +0200

    Initial Commit

---
+++ DataFetcherACNH/DataFetcherACNHApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataFetcherACNHApp.swift
+//  DataFetcherACNH
+//
+//  Created by user on 24/06/22.
+//
+
+import SwiftUI
+
+@main
+struct DataFetcherACNHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
